18908041959aa46cbdb1cb9ae6eb1b6b0a6e5c7f
PhethoMokgalapa <mokayps@gmail.com>
2022-08-31 08:58:31 +0200

    added test cases

--- Spark_project/spark_project/spark_assignment.py
+++ Spark_project/spark_project/spark_assignment.py
@@ -1,7 +1,57 @@
 from pyspark.sql import SparkSession
-def read_from_db():
-    spark=SparkSession\
-        .builder\
-        .appName("Spark Assignment to read from postgress")\
-        .master("local[*]")\
+
+#Connection details
+PSQL_SERVERNAME = "localhost"
+PSQL_PORTNUMBER = 5432
+PSQL_DBNAME = "postgres"
+PSQL_USERNAME = "postgres"
+PSQL_PASSWORD = "pass"
+URL= f"jdbc:postgresql://{PSQL_SERVERNAME}:{PSQL_PORTNUMBER}/{PSQL_DBNAME}"
+TABLE= "books"
+
+
+# reading from CSV to Postgress DB in docker
+
+def write_to_db():
+    spark = SparkSession \
+        .builder \
+        .appName("Spark Assignment to read from postgress") \
+        .master("local[*]") \
         .getOrCreate()
+    df = spark \
+        .read \
+        .format("csv") \
+        .option("header", True)\
+        .option("inferSchema" , True) \
+        .load("C:/dev\projects/Phetho_Assignment/Spark_project/Files/books.csv")
+
+    df.show(5)
+    '''df\
+        .write\
+        .format("jdbc").\
+        option("url" , URL)\
+        .option("dbtable" , TABLE)\
+        .option("user" , PSQL_USERNAME)\
+        .option("password" , PSQL_PASSWORD)\
+        .mode("append")\
+        .save()'''
+    spark.stop()
+
+
+
+#reading from a postgress db
+def read_from_db():
+    spark = SparkSession.builder\
+        .config("spark.jars", "/dev/tools/postgresql-42.5.0.jar") \
+        .master("local")\
+        .appName("PySpark_Postgres_test")\
+        .getOrCreate()
+    df = spark.read.format("jdbc").option("url", "jdbc:postgresql://localhost:5432/postgress") \
+        .option("driver", "org.postgresql.Driver").option("dbtable", "mytable") \
+        .option("user", "postgres").option("password", "pass").load()
+    df.show(2)
+
+    spark.stop()
+if __name__ == "__main__":
+    write_to_db()
+    read_from_db()
--- Spark_project/tests/test_spark_project.py
+++ Spark_project/tests/test_spark_project.py
@@ -1,5 +1,23 @@
-from spark_project.spark_assigment import __version__
+from Spark_project.spark_project import __version__
+from pyspark.sql import SparkSession
+import unittest
 
-
+"test the function that read from postgress db"
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+def test_spark_assignment():
+    spark = SparkSession \
+        .builder \
+        .appName("Spark Assignment to read from postgress") \
+        .master("local[*]") \
+        .getOrCreate()
+
+    df = spark \
+        .read \
+        .format("csv") \
+        .option("header", True) \
+        .option("inferSchema", True) \
+        .load("C:/dev\projects/Phetho_Assignment/Spark_project/Files/books.csv")
+    spark.stop()
+    assert True
